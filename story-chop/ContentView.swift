e4b85705a6a8d9a69d78e39c6e1e2bf4673a28c7
HelenA <helen.g.anderson@me.com>
2025-07-20 18:44:15 +1000

    Initial Commit

---
+++ story-chop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  story-chop
+//
+//  Created by Helen Anderson on 20/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ story-chop/story_chopApp.swift
@@ -0,0 +1,17 @@
+//
+//  story_chopApp.swift
+//  story-chop
+//
+//  Created by Helen Anderson on 20/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct story_chopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
